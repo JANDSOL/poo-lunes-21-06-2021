ac219ba8004e4685a483d23d9f4105c425347abe
Juan Andrade <jpas.juan23@gmail.com>
2021-06-23 12:17:55 -0500

    Method constructor in Employee() class.

---
+++ employee.py
@@ -0,0 +1,11 @@
+from position import Position
+
+class Employee:
+    sequence = 0
+    
+    def __init__(self, nam, ica, sal, pos):
+        self.id = int
+        self.name = nam
+        self.identity_card = ica
+        self.salary = sal
+        self.position = pos
