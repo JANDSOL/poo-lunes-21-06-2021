cd068df53dc72724e519e2f2c21636f6f52b8bf4
Juan Andrade <jpas.juan23@gmail.com>
2021-06-23 12:08:38 -0500

    Show information + end edition.

--- position.py
+++ position.py
@@ -4,3 +4,13 @@ class Position:
         Position.sequence += 1
         self.code = Position.sequence
         self.description = des
+
+
+if __name__ == '__main__':
+    position1 = Position()
+    print(position1.code, position1.description)
+    position2 = Position()
+    position2.description='Director'
+    print(position2.code, position2.description)
+    position3 = Position('Analista')
+    print(position3.code, position3.description)
