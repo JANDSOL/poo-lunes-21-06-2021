b6aba63c5534002516f27230ba556c89ee356d77
Juan Andrade <jpas.juan23@gmail.com>
2021-06-23 12:32:16 -0500

    Method generate_code in Employee() class.

--- employee.py
+++ employee.py
@@ -2,10 +2,14 @@ from position import Position
 
 class Employee:
     sequence = 0
-    
+
     def __init__(self, nam, ica, sal, pos):
-        self.id = int
+        self.id = self.generate_code()
         self.name = nam
         self.identity_card = ica
         self.salary = sal
         self.position = pos
+
+    def generate_code(self):
+        Employee.sequence += 1
+        return Employee.sequence
